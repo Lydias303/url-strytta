cf6d77eafecfcb5722613ee8b43a9f2039a23711
Lydias303 <shaferlydia@yahoo.com>
2015-03-23 14:49:44 -0600

    added nested controller and added routes

---
+++ app/controllers/api/v1/url_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UrlController < ApplicationController
+  respond_to :json, :xml
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  end
+end
