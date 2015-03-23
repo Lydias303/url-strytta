636eea49c9b6b9f0538d9641759deb4577208629
Lydias303 <shaferlydia@yahoo.com>
2015-03-23 17:31:10 -0600

    wrote method for shortener, added before create action

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,12 @@
 class Url < ActiveRecord::Base
+  before_create :generate_shortened_url
 
-  
+  def generate_shortened_url
+    self.shortened_url = self.id.to_s(36)
+    self.save
+  end
+
+  def display_shortened_url
+    ENV['BASE_URL'] + self.shortened_url
+  end
 end
